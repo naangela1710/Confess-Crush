7ccb13fef7cd8a219c4aedc9f7cf9996c828e423
naangela1710 <93580222+naangela1710@users.noreply.github.com>
2021-11-04 16:28:57 +0700

    Create config.js

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
 const CONFIG = {
     titleWeb: "Quà sinh nhật",
     introTitle: 'Đoán xem tui là ai nè :)',
-    introDesc: `Chúc H sinh nhật vui vẻ nhé`,
+    introDesc: 'Chúc H sinh nhật vui vẻ nhé',
     btnIntro: '^^ Nhận quà đi nè hihi ^^',
-    title: 'Tìch tịch tình tang - Con cua bò ngang vạn kiếp
-    Làm bạn thân miết anh không thấy chán à?
-    Tìch tịch tình tang - Tim anh sao cứ mãi hoài lang thang
-    Về nhà ở với em đi được không?',
+    title: 'Tìch tịch tình tang - Con cua bò ngang vạn kiếp.Làm bạn thân miết anh không thấy chán à? Tìch tịch tình tang - Tim anh sao cứ mãi hoài lang thang.Về nhà ở với em đi được không?',
     desc: ' Mình thích H nhiều lắm. H làm người yêu mình nhé <3 Nếu H thoát ra thì cậu ĐỒNG Ý với mình rồi đó ',
     btnYes: 'Đồng ý hau tay hai chân luôn <3',
     btnNo: 'Không nha. Còn lâu nhá :)',
@@ -16,5 +13,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Inbox mình để nhận người iu to bự nhaa ',
     btnAccept: 'Okee luôn <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100017152746196',
-};
+    messLink: 'https://www.facebook.com/profile.php?id=100017152746196'
+}
